b6bdb51639b8f0bd6266abaa8fd745336ff0aadc
Markus Monz <markusmonz@googlemail.com>
2015-08-26 12:45:24 +0200

    improved code readability

--- ctcom_client.m
+++ ctcom_client.m
@@ -65,18 +65,24 @@ try
     log.info('Send CTCOM connection message');
     % send connection request
     client.sendConnectionRequest(request)
+    
     log.info('Receive CTCOM connection acknowledge message');
     % receive connection request acknowledgement message
     ack = client.getMessage();
+    
     % check if received message is valid
     if isempty(ack)
+        
         log.warning('Received CTCOM connection acknowledge message was invalid');
         % close TCP connection
         client.close();
         error('Received message was invalid');
+        
     elseif ack.getType() == MessageType.CONNECT
+        
         log.info('Successfully received CTCOM acknowledge message');
         log.info('Check if CTCOM protocol version matches');
+        
         % check if protcol version matches
         localProtocolVersion = request.getProtocolVersion();
         remoteProtocolVersion = ack.getProtocolVersion();
@@ -85,30 +91,40 @@ try
             fprintf('Protocol versions did not match, local: %s remote: %s', ...
                 localProtocolVersion, remoteProtocolVersion);
         end
+        
         % connection established, wait for the server to send readData
         % message, if protocol version did not match receive the quit
         % message from the ctcom server
         while true
+            
             log.info('Receive next CTCOM message from the CTCOM server');
             message = client.getMessage();
+            
             % check if received message is valid
             if isempty(message)
+                
                 log.warning('Received CTCOM message was invalid');
                 continue;
+                
             elseif message.getType() == MessageType.READ_DATA
+                
                 log.info('Received CTCOM readData message');
                 % get file location from network share
                 ctmatSource = char(message.getLocation());
+                
                 log.info('Load CTMAT file from received location');
                 % load received new data
                 [~,filename,ext] = fileparts(ctmatSource);
                 ctmatData = load(ctmatSource,'-mat');
+                
                 log.info('Rate received ctmat data');
                 % rate received ctmat data
                 ratedCtmatData = rateCtmatData(ctmatData.ctData, algorithm, fixedResult);
+                
                 log.info('Save rated ctmat data');
                 % save new ctmat data to network share
                 [ratedCtmatPath, counter] = saveCtmatFile(ratedCtmatData, ctmatNetworkPath, counter);
+                
                 log.info('Send new CTCOM readData message to server, inform about new CTMAT file');
                 % return updated ctmat file
                 returnMessage = ReadDataMessage();
@@ -116,9 +132,11 @@ try
                 client.sendMessage(returnMessage);
                 
             elseif message.getType() == MessageType.QUIT
+                
                 log.info('Received CTCOM quit message');
                 fprintf('Quit: %s \n', char(message.getMessage()));
                 break;
+                
             end
         end
     end
